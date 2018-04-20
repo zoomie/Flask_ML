c234abd3a63dd7bd32d90b165afb02520e92a7f0
zoomie <z.arderne@gmail.com>
2018-04-20 08:16:51 +0200

    Reads patest.csv file and outputs the result on the machine learing on local host

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
-import pickle
+import dill as pickle
+import pandas as pd
+from Machine_Learning import Predict_df
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def Prediction():
-    tempclf = pickle.load(open('predict.p','rb'))
-    x = [5,6]
+    df_test = pd.read_csv('test.csv')
+    x = Predict_df(df_test)
     return render_template('home.html', x = x)
 
 if __name__ == '__main__':
